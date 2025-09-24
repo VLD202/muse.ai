7bfdaa08aa9e93d86e56eca445df16b39637bf03
Vijaylaxmi dixit <dixitviaylaxmi202@gmail.com>
2025-09-24 16:31:46 +0530

    added codes

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,5 +1,5 @@
 import logo from "./logo.svg";
-import gradientBackground from "./gradientBackground.png";
+import background from "./background.png";
 import user_group from "./user_group.png";
 import star_icon from "./star_icon.svg";
 import star_dull_icon from "./star_dull_icon.svg";
@@ -12,7 +12,7 @@ import ai_gen_img_3 from "./ai_gen_img_3.png";
 
 export const assets = {
     logo,
-    gradientBackground,
+    background,
     user_group,
     star_icon,
     star_dull_icon,
@@ -25,35 +25,35 @@ export const AiToolsData = [
         title: 'AI Article Writer',
         description: 'Generate high-quality, engaging articles on any topic with our AI writing technology.',
         Icon: SquarePen,
-        bg: { from: '#3588F2', to: '#0BB0D7' },
+        bg: { from: '#3588F2', to: '#11778eff' },
         path: '/ai/write-article'
     },
     {
         title: 'Blog Title Generator',
         description: 'Find the perfect, catchy title for your blog posts with our AI-powered generator.',
         Icon: Hash,
-        bg: { from: '#B153EA', to: '#E549A3' },
+        bg: { from: '#B153EA', to: '#90135cff' },
         path: '/ai/blog-titles'
     },
     {
         title: 'AI Image Generation',
         description: 'Create stunning visuals with our AI image generation tool, Experience the power of AI ',
         Icon: Image,
-        bg: { from: '#20C363', to: '#11B97E' },
+        bg: { from: '#063519ff', to: '#11B97E' },
         path: '/ai/generate-images'
     },
     {
         title: 'Background Removal',
         description: 'Effortlessly remove backgrounds from your images with our AI-driven tool.',
         Icon: Eraser,
-        bg: { from: '#F76C1C', to: '#F04A3C' },
+        bg: { from: '#F76C1C', to: '#991c10ff' },
         path: '/ai/remove-background'
     },
     {
         title: 'Object Removal',
         description: 'Remove unwanted objects from your images seamlessly with our AI object removal tool.',
         Icon: Scissors,
-        bg: { from: '#5C6AF1', to: '#427DF5' },
+        bg: { from: '#5C6AF1', to: '#0f47b6ff' },
         path: '/ai/remove-object'
     },
     {
